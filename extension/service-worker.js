7691ad323de2c8ba73451baca9991b31ebcbfcbd
mrryanecurrie-code <mrryanecurrie@gmail.com>
2026-08-18 17:38:53 -0400

    Open Product Finder side panel from toolbar icon

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,1 +1,6 @@
-chrome.runtime.onInstalled.addListener(async()=>{const c=await chrome.storage.local.get(['apiBase']);if(!c.apiBase)await chrome.storage.local.set({apiBase:'http://localhost:3000'})});
+chrome.runtime.onInstalled.addListener(async()=>{
+  const c=await chrome.storage.local.get(['apiBase']);
+  if(!c.apiBase)await chrome.storage.local.set({apiBase:'http://localhost:3000'});
+  await chrome.sidePanel.setPanelBehavior({openPanelOnActionClick:true});
+});
+chrome.runtime.onStartup.addListener(()=>chrome.sidePanel.setPanelBehavior({openPanelOnActionClick:true}).catch(()=>{}));
